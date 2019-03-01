ef17306397f47d61b9df5d06f22f8a79aff27106
Brayder Berroteran <brayderb@gmail.com>
2019-02-28 22:00:24 -0300

    first commit

---
+++ ejercicio2.js
@@ -0,0 +1,3 @@
+var array = ['enero','febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre','octubre','noviembre','diciembre']
+  
+for(var i = 0; i < array.length; i++) { console.log(array[i])}
